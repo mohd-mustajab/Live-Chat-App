751afd858045f2b2c0cf9395555934524a799a35
Mohd Mustajab <mohdmustajab002@gmail.com>
2024-08-11 15:33:30 +0530

    ../

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
--- Backend/routes/Auth.js
+++ Backend/routes/Auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const authMiddleware = require('../Middleware/authMiddleware');
