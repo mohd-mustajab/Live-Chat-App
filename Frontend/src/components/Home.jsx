690753e313f44c7e14e9395b458bfa50334baae2
Mohd Mustajab <mohdmustajab002@gmail.com>
2024-08-15 01:19:04 +0530

    ./

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Home = ({username}) => (
-    <div className='mainpg'style={{backdropFilter: 'blur(10px)'}} >
-      <h1>Welcome to the Chat Application Mr.{username} </h1>
-      <p>App where u can create a temporary chat room and chat with your friend</p>
+    <div className='mainpg' >
+      <h1 style={{backdropFilter: 'blur(10px)'}}>Welcome to the Chat Application Mr.{username} </h1>
+      <p style={{backdropFilter: 'blur(10px)'}}>App where u can create a temporary chat room and chat with your friend</p>
     </div>
   );
 
