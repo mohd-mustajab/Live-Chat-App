b1eb34e89d4222fca087de438b94cc36dd4631fd
Mohd Mustajab <mohdmustajab002@gmail.com>
2024-08-15 00:47:00 +0530

    ./

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
 const Home = ({username}) => (
-    <div className='mainpg'>
-      <div className="container">
+    <div className='home-pg'>
       <h1>Welcome to the Chat Application Mr.{username} </h1>
       <p>App where u can create a temporary chat room and chat with your friend</p>
-    </div>
     </div>
   );
 
