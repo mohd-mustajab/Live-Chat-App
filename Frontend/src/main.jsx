4af0c481be4575117343c472e008b75cfd83df01
Mohd Mustajab <mohdmustajab002@gmail.com>
2024-09-06 10:28:25 +0530

    bb

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store'; // Adjust the path if necessary
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   
+      <App />
+    
+  </React.StrictMode>
 );
