d1040f989ac1222220f88433261815bf8ffafa4e
Mohd Mustajab <mohdmustajab002@gmail.com>
2025-06-05 15:40:55 +0530

    aaa

--- Backend/socketClient.js
+++ Backend/socketClient.js
@@ -12,6 +12,10 @@ socket.on('connect', () => {
 socket.on('disconnect', (reason) => {
   console.log('Disconnected from server:', reason);
 });
+socket.on("chatMessage", async ({ roomId, userId, message }) => {
+  const username = await getUserNameById(userId); // Make sure it's awaited
+  io.to(roomId).emit("message", { username, message });
+});
 
 socket.on('error', (error) => {
   console.error('Socket.IO error:', error);
