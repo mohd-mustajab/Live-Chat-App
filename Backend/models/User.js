b47f4108a2b5308000650f9d8b4af64110cb2993
Mohd Mustajab <mohdmustajab002@gmail.com>
2025-06-08 16:01:59 +0530

    update user.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); // ✅ Add this
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
@@ -22,14 +23,29 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// ✅ Hash password before saving (if not already hashed)
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
 
-// Generate JWT
-userSchema.methods.generateAuthToken = function() {
-  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// ✅ Compare entered password with hashed password
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
+// ✅ Generate JWT
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
